b51ec54e146c8859048e8dd32c0cb3bbc226a33c
Corbin Otwell <cbotwell@gmail.com>
2015-07-31 13:59:10 -0500

    got templates for game screens set up and calling the battle screen

--- assets/js/cagematch.js
+++ assets/js/cagematch.js
@@ -3,9 +3,10 @@ memphisRaines = new Character({name: 'Memphis Raines', weapons: {eleanor: 60}, h
 benjaminGates = new Character({name: 'Benjamin Gates', weapons: {declarationOfIndependence: 50}, hitPoints: 100});
 johhnyBlaze = new Character({name: 'Johnny Blaze', weapons: {chain: 30}, hitPoints: 100});
 
+$('.game-target').html(AppTemplates.battle());
+
 var heroTargetEl = $('.hero-target');
 var enemyTargetEl = $('.enemy-target');
-
 
 var renderHero = function(output) {
   var results = AppTemplates.character(output);
