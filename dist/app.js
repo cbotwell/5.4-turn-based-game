fb270cfe69d663d0376267a555633d138413d53d
Corbin Otwell <cbotwell@gmail.com>
2015-08-02 23:29:20 -0500

    deploying

---
+++ dist/app.js
@@ -0,0 +1,155 @@
+AppTemplates = {};
+
+AppTemplates['battle'] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1, alias1=this.lambda, alias2=this.escapeExpression;
+
+  return "<div class=\"center\">\n    <div class=\"battle\">\n        <div class=\"characters\">\n            <div class=\"hero-target\">\n                <img src=\""
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.imgUrl : stack1), depth0))
+    + "\" alt=\""
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.name : stack1), depth0))
+    + "\">\n                <p>"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.name : stack1), depth0))
+    + "</p>\n                <div class=\"hp\">\n                    <p>HP: "
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.getHealth : stack1), depth0))
+    + "</p>\n                    <div class=\"hpborder\">\n                        <div class=\"heroHpBar\"></div>\n                    </div>\n                </div>\n            </div>\n            <div class=\"enemy-target\">\n                <img src=\""
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.imgUrl : stack1), depth0))
+    + "\" alt=\""
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.name : stack1), depth0))
+    + "\">\n                <p>"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.name : stack1), depth0))
+    + "</p>\n                <div class=\"hp\">\n                    <p>HP: "
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.enemy : depth0)) != null ? stack1.getHealth : stack1), depth0))
+    + "</p>\n                    <div class=\"hpborder\">\n                        <div class=\"enemyHpBar\"></div>\n                    </div>\n                </div>\n            </div>\n        </div>\n        <div class=\"messages\">\n            <div class=\"fight\">\n                <i class=\"fa fa-play\"></i>\n                <button class=\"attack\">"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.hero : depth0)) != null ? stack1.weaponName : stack1), depth0))
+    + "</button>\n            </div>\n        </div>\n    </div>\n</div>\n";
+},"useData":true});
+AppTemplates['gameover'] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<button class=\"retry\">\n    Play Again\n</button>\n<div class=\"center\">\n    <div class=\"gameover\">\n        <h1>CAGE WINS!</h1>\n        <p>\n            \"I don't think movies are the reason why this violence exists, I think it's going to happen whether movies are there or not.\" - Nicolas Cage\n        </p>\n    </div>\n</div>\n";
+},"useData":true});
+AppTemplates['start'] = Handlebars.template({"1":function(depth0,helpers,partials,data,blockParams) {
+    var stack1, helper, alias1=this.lambda, alias2=this.escapeExpression;
+
+  return "                <div class=\"character\">\n                    <img src=\""
+    + alias2(alias1(((stack1 = blockParams[0][0]) != null ? stack1.imgUrl : stack1), depth0))
+    + "\" alt=\""
+    + alias2(alias1(((stack1 = blockParams[0][0]) != null ? stack1.name : stack1), depth0))
+    + "\">\n                    <p>"
+    + alias2(alias1(((stack1 = blockParams[0][0]) != null ? stack1.name : stack1), depth0))
+    + "</p>\n                    <button class=\"select-character\" data-index=\""
+    + alias2(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"index","hash":{},"data":data,"blockParams":blockParams}) : helper)))
+    + "\"> Select</button>\n                </div>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data,blockParams) {
+    var stack1;
+
+  return "<div class=\"center\">\n    <div class=\"start\">\n        <h1>CAGE MATCH</h1>\n        <div class=\"characters\">\n"
+    + ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 1, blockParams),"inverse":this.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
+    + "        </div>\n    </div>\n</div>\n";
+},"useData":true,"useBlockParams":true});
+function Character(options) {
+  options = options || {};
+  var hitPoints = options.hitPoints || 100;
+  this.weapons = options.weapons || {};
+  this.weaponName = options.weaponName;
+  this.name = options.name;
+  this.imgUrl = options.imgUrl;
+
+  this.takeDamage = function(damage) { hitPoints -=  damage; };
+
+  this.getAttackStrength = function(weaponName) {
+    if (this.weapons[weaponName]) {
+      return this.weapons[weaponName];
+    }
+
+    return 5;
+  };
+
+  this.on('attacked', function(amount) {
+    this.takeDamage(amount);
+  });
+
+  this.getHealth = function() {return hitPoints;};
+}
+
+Character.prototype = _.extend({
+  constructor: Character,
+
+  attack: function(hostile, weapon) {
+    hostile.trigger('attacked', this.getAttackStrength(weapon));
+  }
+
+}, Backbone.Events);
+
+castorTroy = new Character({name: 'Castor Troy', weapons: {dualGoldenGuns: 40}, weaponName: 'Dual Golden Guns', hitPoints: 100, imgUrl: 'castor.png'});
+memphisRaines = new Character({name: 'Memphis Raines', weapons: {eleanor: 60}, weaponName: 'Eleanor', hitPoints: 100, imgUrl: 'nic.png'});
+benjaminGates = new Character({name: 'Benjamin Gates', weapons: {declarationOfIndependence: 50}, weaponName: 'Declaration of Independence', hitPoints: 100, imgUrl: 'nic.png'});
+johhnyBlaze = new Character({name: 'Johnny Blaze', weapons: {chain: 30}, weaponName: 'Chain', hitPoints: 100, imgUrl: 'nic.png'});
+
+var heros = [castorTroy, memphisRaines, benjaminGates, johhnyBlaze];
+var gameTargetEl = $('.game-target');
+
+gameTargetEl.html(AppTemplates.start(heros));
+
+function Game(hero) {
+  this.hero = hero;
+  this.enemy = johhnyBlaze;
+  this.turnNumber = 0;
+  this.gameOver = false;
+}
+
+Game.prototype = _.extend({
+  constructor: Game,
+
+  isGameOver: function() {
+    //something?
+  }
+}, Backbone.Events);
+
+gameTargetEl.on('click', '.select-character', function() {
+  var indexSelected = $(this).data('index');
+  var hero = heros[indexSelected];
+  var game = new Game(hero);
+  newBattle(game);
+});
+
+var healthBar;
+
+var newBattle = function(game) {
+  gameTargetEl.html(AppTemplates.battle(game));
+  healthBar = $('.enemyHpBar');
+  healthBar.width(game.enemy.getHealth() + '%');
+
+  gameTargetEl.on('click', '.attack', function() {
+    game.hero.attack(game.enemy, 'dualGoldenGuns');
+    game.turnNumber++;
+
+    if (game.turnNumber % 2 == 1) {
+      $('.messages').html('Enemy Cage attacks with ' + game.enemy.weaponName);
+      window.setTimeout(function() {
+        game.enemy.attack(game.hero, 'chain');
+        game.turnNumber++;
+      }, 5000);
+    }
+
+    if (game.enemy.getHealth() <= 0 || game.hero.getHealth() <= 0) {
+      gameTargetEl.html(AppTemplates.gameover());
+    } else {
+      gameTargetEl.html(AppTemplates.battle(game));
+      healthBar = $('.enemyHpBar');
+      healthBar.width(game.enemy.getHealth() + '%');
+      if (game.enemy.getHealth() <= 20) {
+        healthBar.addClass('lowHealth');
+      }
+    }
+  });
+
+  gameTargetEl.on('click', '.retry', function() {
+    gameTargetEl.html(AppTemplates.start(heros));
+  });
+
+  //trying to figure out how to use the below stuff later
+
+  if (game.gameOver) {
+    $('.game-target').html(AppTemplates.gameover());
+  }
+};
+//# sourceMappingURL=app.map
